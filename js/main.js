457b4f9382a3952e1a139981dd5a1f1bfb496b9e
Dennis Christman <dennis.christman@gmail.com>
2017-02-14 10:59:09 -0500

    Grid is now scalable and resettable to user input dimensions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function (){
-  var blockSize = 550/(16);
-  blockSize = blockSize + "px";
-  console.log(blockSize);
-  $('.block').css({"height": blockSize, "width": blockSize});
-  for(var i=0; i<16; i++){
-    $('#sketchpad').append('<div class="line"></div>');
-      for (var j=0; j<16; j++){
-        $('.line:last-child').append('<div class="block"></div>')
+  resetGrid(16);
+});
+
+var resetGrid = function (){
+  $('#sketchpad').empty();
+  var gridSize = $('input:text').val()
+  var blockSize = 100 / gridSize;
+  blockSize = blockSize + '%';
+  for(var i=0; i<gridSize; i++){
+      for (var j=0; j<gridSize; j++){
+        $('#sketchpad').append('<div class="block"></div>')
     }
   }
   $('.block').css({"height": blockSize, "width": blockSize});
-});
+}
