b21c19114368ffde21195f90520c06f72976c6b6
Dennis Christman <dennis.christman@gmail.com>
2017-02-14 11:18:22 -0500

    Finished basic features. Onwards!

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function (){
-  resetGrid(16);
+  //Create initial grid
+  resetGrid();
+
 });
 
 var resetGrid = function (){
+  //clear grid
   $('#sketchpad').empty();
+  //set the grid dimensions, use it to set the block dimensions
   var gridSize = $('input:text').val()
   var blockSize = 100 / gridSize;
   blockSize = blockSize + '%';
+  //loop to create grid
   for(var i=0; i<gridSize; i++){
       for (var j=0; j<gridSize; j++){
         $('#sketchpad').append('<div class="block"></div>')
     }
   }
+  //set size of grid blocks
   $('.block').css({"height": blockSize, "width": blockSize});
-}
+  //create function to draw on the grid
+  $('.block').mouseenter(function (){
+    $(this).css('background-color','black');
+  });
+};
